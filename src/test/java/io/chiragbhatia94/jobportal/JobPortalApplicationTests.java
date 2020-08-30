f743a3d9330895c7e9ef83a45a060c52fb6d9005
Chirag Bhatia <chiragbhatia94@gmail.com>
2020-08-30 21:06:01 +0530

    feat(rest): added spring boot starter project

---
+++ src/test/java/io/chiragbhatia94/jobportal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chiragbhatia94.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
