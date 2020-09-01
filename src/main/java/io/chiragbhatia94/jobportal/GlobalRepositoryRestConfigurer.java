0e54c818289fd3f30c0b7f94f668d53a915e63e7
Chirag Bhatia <chiragbhatia94@gmail.com>
2020-09-01 20:47:52 +0530

    feat: added cors configuration

---
+++ src/main/java/io/chiragbhatia94/jobportal/GlobalRepositoryRestConfigurer.java
@@ -0,0 +1,20 @@
+package io.chiragbhatia94.jobportal;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class GlobalRepositoryRestConfigurer implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.getCorsRegistry()
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
