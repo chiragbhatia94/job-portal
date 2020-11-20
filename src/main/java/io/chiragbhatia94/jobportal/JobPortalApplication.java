32cc39319216ae32a833c0e35ca96ac8730a8a7b
Chirag Bhatia <chiragbhatia94@gmail.com>
2020-11-20 18:16:21 +0530

    fix: fixed git2consul

--- src/main/java/io/chiragbhatia94/jobportal/JobPortalApplication.java
+++ src/main/java/io/chiragbhatia94/jobportal/JobPortalApplication.java
@@ -2,12 +2,14 @@ package io.chiragbhatia94.jobportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @EnableDiscoveryClient
 @EnableSwagger2
 @Import({ SpringDataRestConfiguration.class })
--- src/main/java/io/chiragbhatia94/jobportal/config/MetaProperties.java
+++ src/main/java/io/chiragbhatia94/jobportal/config/MetaProperties.java
@@ -2,15 +2,16 @@ package io.chiragbhatia94.jobportal.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @RefreshScope
 @Configuration
-@ConfigurationProperties()
 @Getter
 @Setter
 public class MetaProperties {
+    @Value("${port}")
     private String port;
 }
